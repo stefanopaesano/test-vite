a1e702dfd0e5df6d62e329bd16516bff23806e3b
stefano paesano <stefanopaesano022@gmail.com>
2024-01-17 13:25:13 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  data() {
+    return  {count: 0}
+  },
+  
+};
+
+</script>
+
+<template>
+ <div>
+      <h1> 
+        ciao
+      </h1>
+      <p>
+      {{ count }}
+      </p>
+      <button @click="count++">
+      incre count
+      </button>
+  </div> 
+</template>
+
+<style scoped>
+
+</style>
